6193a5b75063d4d4e277859a3ed1b741d95c5f5d
oliviaalchemy <olivia@alchemytechs.com>
2019-08-07 11:49:18 +0100

    Add project files.

---
+++ While Loops/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace While_Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int index = 6;
+            do                   //do while loop will execute the code inside of the loop before it checks the condition. 1) checks while(index<=5) then int index=6
+            {
+                Console.WriteLine(index);
+                index++;
+            } while (index <= 5);
+
+                Console.ReadLine(); 
+        }
+    }
+}
